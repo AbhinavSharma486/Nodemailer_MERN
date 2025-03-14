a06a799a2c7fcc6547587b394e2a947e006abccf
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-03-14 11:05:52 +0530

    login controller complete

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -42,3 +42,42 @@ export const register = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({ success: false, message: "Email & Password are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d"
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    return res.json({ success: true, message: "User logges in successfully" });
+
+
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+}
+
