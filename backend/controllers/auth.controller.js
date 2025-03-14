7450aba87cd3025e35580536cb82a181219332e3
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-03-14 10:58:58 +0530

    signup controller complete

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "Please enter all fields" });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d"
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
