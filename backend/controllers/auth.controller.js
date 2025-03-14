ee8becd7dbfbd16b61e49b11760f7891fec2907f
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-03-14 11:11:28 +0530

    logout controller complete

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -79,5 +79,19 @@ export const login = async (req, res) => {
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
-}
+};
 
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    });
+
+    return res.json({ success: true, message: "User logged out successfully" });
+
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
